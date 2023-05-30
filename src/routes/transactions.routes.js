88a4957200a5aa5c0b8f502e0e84f557ad3d0f3b
LiviaCCO <livia.ccapistrano@gmail.com>
2023-05-30 09:33:49 -0300

    schemas feito!

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -2,14 +2,14 @@ import { Router } from "express";
 import { createShorten, getId, getUrl, deleteId, getUser, getRanking } from "../controllers/transaction.controllers.js";
 import validateSchema from "../middlewares/validateSchema.middleware.js";
 import authValidation from "../middlewares/authValidation.middleware.js";
-import { urlSchema, idSchema } from "../schemas/schemas.js";
+import { urlSchema } from "../schemas/schemas.js";
 
 const transactionsRouter = Router();
 
 transactionsRouter.post("/urls/shorten", authValidation, validateSchema(urlSchema), createShorten);
 transactionsRouter.get("/urls/:id", getId);
 transactionsRouter.get("/urls/open/:shortUrl", getUrl);
-transactionsRouter.delete("/urls/:id", authValidation, validateSchema(idSchema), deleteId);
+transactionsRouter.delete("/urls/:id", authValidation, deleteId);
 transactionsRouter.get("/users/me", authValidation, getUser);
 transactionsRouter.get("/ranking", getRanking);
 
